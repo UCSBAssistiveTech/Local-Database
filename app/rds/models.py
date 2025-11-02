4358b40b3531ae6f2ee0e6b52406351641571d36
joshL1215 <210241055+joshL1215@users.noreply.github.com>
2025-11-01 17:17:10 -0700

    Add Exam table and foreign key to db model

--- app/rds/models.py
+++ app/rds/models.py
@@ -1,5 +1,6 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
@@ -10,8 +11,18 @@ class Patient(Base):
     id = Column(Integer, primary_key=True, Index=True)
     name = Column(String, nullable=False)
     age = Column(Integer, nullable=False)
+
+    exams = relationship("Exam", back_populates="patient")
+
+
+class Exam(Base):
+
+    __tablename__ = 'exams'
+
+    id = Column(Integer, primary_key=True, Index=True)
     reaction_time = Column(Float, nullable=False)
     pupil_dialation = Column(Float, nullable=False)
     eye_velocity = Column(Float, nullable=False)
 
-
+    patient_id = Column(Integer, ForeignKey('patients.id'), nullable=False)
+    patient = relationship("Patient", back_populates="exams")
