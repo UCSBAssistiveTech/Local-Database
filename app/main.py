0fd7df90f493cf0edbe6ecf37d99deb639552728
joshL1215 <210241055+joshL1215@users.noreply.github.com>
2025-11-02 22:05:49 -0800

    add uvicorn thing

--- app/main.py
+++ app/main.py
@@ -2,7 +2,10 @@ from fastapi import FastAPI
 from app.rds.db import engine
 import app.rds.models as models
 
+import uvicorn
+
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
-# app.include_router(api.routes.router)
 
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
